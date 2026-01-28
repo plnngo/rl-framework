d6277a13bd0c32e58fb840ed8fe4a75040e3b42d
plnngo <p.l.n.ngo@tudelft.nl>
2026-01-28 16:27:26 +0100

    Call heuristic macro

--- main_macro.py
+++ main_macro.py
@@ -5,11 +5,12 @@ from stable_baselines3 import DQN, PPO
 from tqdm import trange
 import MCTSenv
 from MacroEnv import MacroEnv, _sync_envs
+from deterministic_tracker import select_best_macro_action
 from main import analyse_tracking_task, plot_detection_bar_chart, plot_means_lost_targets, plot_violin
 from multi_seed_training_macro import MacroRandomSeedEnv
 from multi_target_env import MultiTargetEnv
 
-def evaluate_agent_macro(seeds=None, env=None, model=None, n_episodes=100, random_policy=False, n_targets=5, n_unknown_targets=100):
+def evaluate_agent_macro(seeds=None, env=None, model=None, n_episodes=100, random_policy=False, deterministic_policy=False,):
     rewards = []
     detect_count3 = 0
     detection_count = []
@@ -24,7 +25,8 @@ def evaluate_agent_macro(seeds=None, env=None, model=None, n_episodes=100, rando
     known = []
 
     for ep in trange(n_episodes, desc="Evaluating",  leave=False):
-        obs, _ = env.reset(seeds)
+        seed = int(np.random.choice(seeds))
+        obs, _ = env.reset(seed=seed)
         exceed_target = []
         if ep == n_episodes - 1:
             last_env = copy.deepcopy(env)
@@ -38,51 +40,55 @@ def evaluate_agent_macro(seeds=None, env=None, model=None, n_episodes=100, rando
         while not done:
 
             if isinstance(model, MCTSenv.MCTS):
-                next_obs, reward, done, truncated, info, action, micro = run_mcts_step(env, model, obs)
+                obs, reward, done, truncated, info, action, micro = run_mcts_step(env, model, obs)
+            elif deterministic_policy:
+                action = select_best_macro_action(env)
+                obs, reward, done, truncated, info = env.step(action)
+
             else:
                 if random_policy:
                     action = env.action_space.sample()
                 else:
                     action, _ = model.predict(obs, deterministic=False)
-                next_obs, reward, done, truncated, info = env.step(action)
+                obs, reward, done, truncated, info = env.step(action)
 
             total_reward += reward
 
             # evaluate searching task
-            known_targets = sum(info["action_mask"]["micro_track"])
+            """ known_targets = sum(info["action_mask"]["micro_track"])
             if known_targets>detections:
                 detect_count3 = detect_count3 + (known_targets - detections)
-                detections = known_targets
+                detections = known_targets """
 
             # evaluate tracking task
             if ep == n_episodes - 1:
                 episode_log[t] = {}
                 episode_task_log[t] = {"timestamp": t, "action": int(action)}
             if ep == n_episodes - 1:
-                for tgt in range(env.base_env.n_targets):
+                """ for tgt in range(env.base_env.n_targets):
                     #print(env.base_env.targets[tgt]["id"])
                     exceed, x, P = analyse_tracking_task(next_obs, env.base_env.targets[tgt]["id"], env.base_env, confidence=0.95)
-                    """ if exceed and tgt not in exceed_target:
+                    if exceed and tgt not in exceed_target:
                         exceed_target.append(tgt)
-    """
+   
                     # if this is the last episode, store everything
                     if tgt == info["target_id"]:
                         episode_log[t][tgt] = {
                             "id": tgt,
                             "state": x.copy(),
                             "cov": P.copy()
-                        }
+                        } """
 
             t += 1  # increment timestep
         rewards.append(total_reward)
-        detection_count.append(env.base_env.detect_counter)
-        exceedFOV.append(env.base_env.lost_counter)
-        known.append(env.base_env.n_targets)
+        detection_count.append(env.detect_counter)
+        exceedFOV.append(env.lost_counter)
+        known.append(env.n_targets)
         # --- For last episode, store deep copy of env ---
         if ep == n_episodes - 1:
             last_episode_log = episode_log
             last_tasks_log = episode_task_log
-    return rewards, detection_count, detect_count3, exceedFOV, last_env, last_episode_log, known, last_tasks_log
+    return rewards, detection_count, exceedFOV, last_env, last_episode_log, known, last_tasks_log
 
 
 
@@ -130,7 +136,7 @@ def plot_pareto(detection_count, exceedFOV):
 
     return pareto_idx, pareto_points
 
-def plot_pareto_multi(algorithms):
+def plot_pareto_multi(algorithms, color_map=None):
     """
     algorithms: list of tuples (detection_count, exceedFOV, label)
         detection_count: list of objective 1 values (maximize)
@@ -145,7 +151,10 @@ def plot_pareto_multi(algorithms):
     ])
     """
     
-     # --- build global list of points with algorithm index ---
+    if color_map is None:
+        color_map = {}
+
+    # --- build global list of points with algorithm index ---
     all_points = []
     for a_idx, (det_list, fov_list, label) in enumerate(algorithms):
         assert len(det_list) == len(fov_list)
@@ -166,19 +175,22 @@ def plot_pareto_multi(algorithms):
             pareto_idx.append(i)
 
     # sorted only for drawing the line (but remember original indices)
-    pareto_sorted = sorted(pareto_idx, key=lambda i: all_points[i][1])  # by exceedFOV (x-axis)
+    pareto_sorted = sorted(pareto_idx, key=lambda i: all_points[i][1])
 
     # --- plotting ---
     plt.figure()
 
-    # consistent colors for each algorithm
+    # fallback colormap (only used if label not in color_map)
     cmap = plt.cm.get_cmap("tab10", len(algorithms))
 
     # 1) scatter all points for each algorithm
     for a_idx, (det_list, fov_list, label) in enumerate(algorithms):
+        color = color_map.get(label, cmap(a_idx))
+
         plt.scatter(
-            fov_list, det_list,
-            color=cmap(a_idx),
+            fov_list,
+            det_list,
+            color=color,
             label=label,
             alpha=0.7
         )
@@ -191,7 +203,16 @@ def plot_pareto_multi(algorithms):
     # 3) re-plot the Pareto points individually in their algorithm colors
     for i in pareto_sorted:
         d, e, label, a_idx = all_points[i]
-        plt.scatter(e, d, color=cmap(a_idx), edgecolor="black", s=70, linewidth=0.8)
+        color = color_map.get(label, cmap(a_idx))
+
+        plt.scatter(
+            e,
+            d,
+            color=color,
+            edgecolor="black",
+            s=70,
+            linewidth=0.8
+        )
 
     plt.xlabel("Number of exceed-FOV events")
     plt.ylabel("Detection count")
@@ -220,9 +241,9 @@ def run_mcts_step(env, mcts, obs):
     # Execute the action exactly as in your run_episode code
     if macro == 0:     # SEARCH
         next_obs, reward_vec, done, truncated, info = env.search_env.env.step(micro)
-        _sync_envs(env.search_env.env, env.base_env)
+        _sync_envs(env.search_env.env, env)
         _sync_envs(env.search_env.env, env.track_env.env)
-        trackingNeeded, trackingReallyNeeded = env._compute_track_reward(env.base_env)
+        trackingNeeded, trackingReallyNeeded = env._compute_track_reward(env)
         if (sum(trackingNeeded) + sum(trackingReallyNeeded))>=1:
             # tracking nedded instead of 
             reward = -0.5 * (sum(trackingNeeded) + sum(trackingReallyNeeded))
@@ -230,9 +251,9 @@ def run_mcts_step(env, mcts, obs):
             reward = 1
     else:              # TRACK
         next_obs, reward_vec, done, truncated, info = env.track_env.env.step(micro)
-        _sync_envs(env.track_env.env, env.base_env)
+        _sync_envs(env.track_env.env, env)
         _sync_envs(env.track_env.env, env.search_env.env)
-        trackingNeeded, trackingReallyNeeded = env._compute_track_reward(env.base_env)
+        trackingNeeded, trackingReallyNeeded = env._compute_track_reward(env)
         if any(trackingNeeded):
             reward = 2.0
         elif any(trackingReallyNeeded):
@@ -263,32 +284,37 @@ def plot_task_log(task_log, title="Action Timeline"):
 
 if __name__ == "__main__":
     seeds = [42, 123, 321]
-    n_episodes = 100
+    n_episodes = 10
     n_targets = 5
     n_unknown_targets =100
 
-    envMcts = MacroRandomSeedEnv._make_env(n_targets,n_unknown_targets,seed=int(np.random.choice(seeds)))
+    """ envMcts = MacroRandomSeedEnv._make_env(n_targets,n_unknown_targets,seed=int(np.random.choice(seeds)))
     mcts = MCTSenv.MCTS(env=envMcts, rollout_depth=5, gamma=0.95)
-    rewardsMcts, detection_countMcts, detect_count3Mcts, exceedFOVMcts, last_envMcts, last_episode_logMcts, knownMcts, last_tasks_logMcts = evaluate_agent_macro(seeds=seeds, env=envMcts, model=mcts, n_episodes=n_episodes, random_policy=True, n_targets=n_targets, n_unknown_targets=n_unknown_targets)
-    #plot_pareto(detection_countMcts, exceedFOVMcts)
+    rewardsMcts, detection_countMcts, exceedFOVMcts, last_envMcts, last_episode_logMcts, knownMcts, last_tasks_logMcts = evaluate_agent_macro(seeds=seeds, env=envMcts, model=mcts, n_episodes=n_episodes, random_policy=True, deterministic_policy=False) """
+
+    envHeuristic = MacroRandomSeedEnv._make_env(n_targets,n_unknown_targets,seed=int(np.random.choice(seeds)))
+    rewardsHeuristic, detection_countHeuristic, exceedFOVHeuristic, last_envHeuristic, last_episode_logHeuristic, knownHeuristic, last_tasks_logHeuristic = evaluate_agent_macro(seeds=seeds, env=envHeuristic, model=None, n_episodes=n_episodes, random_policy=False, deterministic_policy=True)
 
     envRandom = MacroRandomSeedEnv._make_env(n_targets,n_unknown_targets,seed=int(np.random.choice(seeds)))
-    rewardsRandom, detection_countRandom, detect_count3Random, exceedFOVRandom, last_envRandom, last_episode_logRandom, knownRandom, last_tasks_logRandom = evaluate_agent_macro(seeds=seeds, env=envRandom, model=None, n_episodes=n_episodes, random_policy=True, n_targets=n_targets, n_unknown_targets=n_unknown_targets)
+    rewardsRandom, detection_countRandom, exceedFOVRandom, last_envRandom, last_episode_logRandom, knownRandom, last_tasks_logRandom = evaluate_agent_macro(seeds=seeds, env=envRandom, model=None, n_episodes=n_episodes, random_policy=True, deterministic_policy=False)
 
     envPPO = MacroRandomSeedEnv._make_env(n_targets,n_unknown_targets,seed=int(np.random.choice(seeds)))
-    ppo_model = PPO.load("agents/ppo_macro_shorttrained", env=envPPO)
-    rewardsPPO, detection_countPPO, detect_count3PPO, exceedFOVPPO, last_envPPO, last_episode_logPPO, knownPPO, last_tasks_logPPO = evaluate_agent_macro(seeds=seeds, env=envPPO, model=ppo_model, n_episodes=n_episodes, random_policy=False, n_targets=n_targets, n_unknown_targets=n_unknown_targets)
+    ppo_model = PPO.load("agents/ppo_macro_trained_longer", env=envPPO)
+    rewardsPPO, detection_countPPO, exceedFOVPPO, last_envPPO, last_episode_logPPO, knownPPO, last_tasks_logPPO = evaluate_agent_macro(seeds=seeds, env=envPPO, model=ppo_model, n_episodes=n_episodes, random_policy=False, deterministic_policy=False)
 
     envDQN = MacroRandomSeedEnv._make_env(n_targets,n_unknown_targets,seed=int(np.random.choice(seeds)))
-    dqn_model = DQN.load("agents/dqn_macro_shorttrained", env=envDQN)
-    rewardsDQN, detection_countDQN, detect_count3DQN, exceedFOVDQN, last_envDQN, last_episode_logDQN, knownDQN, last_tasks_logDQN = evaluate_agent_macro(seeds=seeds, env=envDQN, model=dqn_model, n_episodes=n_episodes, random_policy=False, n_targets=n_targets, n_unknown_targets=n_unknown_targets)
+    dqn_model = DQN.load("agents/dqn_macro_trained_rewardLengthObs", env=envDQN)
+    rewardsDQN, detection_countDQN, exceedFOVDQN, last_envDQN, last_episode_logDQN, knownDQN, last_tasks_logDQN = evaluate_agent_macro(seeds=seeds, env=envDQN, model=dqn_model, n_episodes=n_episodes, random_policy=False, deterministic_policy=False)
+
+    #plot_pareto(detection_countMcts, exceedFOVMcts)
 
     # ****** Plot reward distributions ******
     reward_results = {
         "Random": rewardsRandom,
         "PPO": rewardsPPO,
-        "DQN": rewardsDQN,
-        "MCTS": rewardsMcts
+        "Heuristic": rewardsHeuristic,
+        "DQN": rewardsDQN
+        #"MCTS": rewardsMcts
     }
     plot_violin(reward_results, ylabel="Episode Reward")
 
@@ -296,35 +322,41 @@ if __name__ == "__main__":
     detection_results = {
         "Random": detection_countRandom,
         "PPO": detection_countPPO,
+        "Heuristic": detection_countHeuristic,
         "DQN": detection_countDQN,
-        "MCTS": detection_countMcts
+        #"MCTS": detection_countMcts
     }
     plot_violin(detection_results, ylabel="Number of Detections")
 
-    # ****** Plot total detection ******
-    """ detection_results = {
-        "Random": detect_count3Random,
-        "PPO": detect_count3PPO,
-        "DQN": detect_count3DQN
-    }
-    plot_detection_bar_chart(detection_results) """
-
     # ****** Plot total target lost ******
-    plot_means_lost_targets(exceedFOVPPO, knownPPO, exceedFOVDQN, knownDQN, exceedFOVRandom, knownRandom, exceedFOVMcts, knownMcts)
+    #plot_means_lost_targets(ppo=exceedFOVPPO, knownPPO=knownPPO, dqn=[], knownDQN=[], random=exceedFOVRandom, knownRandom=knownRandom, det=[], knowndet=[])
+    plot_means_lost_targets(ppo=exceedFOVPPO, knownPPO=knownPPO, dqn=exceedFOVDQN, knownDQN=knownDQN, random=exceedFOVRandom, knownRandom=knownRandom, det=exceedFOVHeuristic, knowndet=knownHeuristic)
 
     # ****** Plot time spent on task ******
     plot_task_log(last_tasks_logRandom, title="Random Policy - Action Timeline")
     plot_task_log(last_tasks_logPPO, title="PPO - Action Timeline")
     plot_task_log(last_tasks_logDQN, title="DQN - Action Timeline")
-    plot_task_log(last_tasks_logMcts, title="MCTS - Action Timeline")
+    plot_task_log(last_tasks_logHeuristic, title="Heuristic - Action Timeline")
+    #plot_task_log(last_tasks_logMcts, title="MCTS - Action Timeline")
+
 
     # ****** Non-dominated solution plot******
-    plot_pareto_multi([
+    plot_pareto_multi(
+    [
         (detection_countPPO, exceedFOVPPO, 'PPO'),
         (detection_countDQN, exceedFOVDQN, 'DQN'),
-        (detection_countMcts, exceedFOVMcts, 'MCTS'),
+        #(detection_countMcts, exceedFOVMcts, 'MCTS'),
+        (detection_countHeuristic, exceedFOVHeuristic, 'Heuristic'),
         (detection_countRandom, exceedFOVRandom, 'Random')
-    ])
+    ],
+    color_map={
+        'PPO': 'blue',
+        'DQN': 'yellow',
+        'MCTS': 'purple',
+        'Random': 'red',
+        'Heuristic': 'green',
+    }
+)
 
 
 
